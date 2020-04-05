a646e38f7c704bd28228bcb430fb9eda40a9ef50
vudinhquang <vudinhquangk53@gmail.com>
2020-04-05 23:28:50 +0900

    Mô hình MVC, luồng hoạt động của SpringMVC

---
+++ Spring Core/spring_ex/src/main/java/spring/ex/Category.java
@@ -0,0 +1,18 @@
+package spring.ex;
+
+public class Category {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private int id;
+	private String name;
+}
