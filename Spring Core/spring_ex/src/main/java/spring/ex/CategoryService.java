9d346cd5a6bfa2762668f83dad977109faff4ae7
vudinhquang <vudinhquangk53@gmail.com>
2020-04-05 19:19:13 +0900

    Spring Core - Phần 2

--- Spring Core/spring_ex/src/main/java/spring/ex/CategoryService.java
+++ Spring Core/spring_ex/src/main/java/spring/ex/CategoryService.java
@@ -2,14 +2,14 @@ package spring.ex;
 
 public class CategoryService {
 	private ICategory category;
-	public CategoryService() {
-
+	public CategoryService(ICategory category) {
+		this.category = category;
 	}
 	public ICategory getCategory() {
 		return category;
 	}
-	public void setCategory(ICategory category) {
-		this.category = category;
-	}
+//	public void setCategory(ICategory category) {
+//		this.category = category;
+//	}
 	
 }
--- Spring Core/spring_ex/src/main/java/spring/ex/Main.java
+++ Spring Core/spring_ex/src/main/java/spring/ex/Main.java
@@ -1,11 +1,20 @@
 package spring.ex;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 	public static void main(String[] args) {
-		CategoryService service = new CategoryService();
-		service.setCategory(new SamsungCategory());
-		service.getCategory().printCategory();
-		service.setCategory(new AppleCategory());
+		// Dependency Injection
+//		CategoryService service = new CategoryService();
+//		service.setCategory(new SamsungCategory());
+//		service.getCategory().printCategory();
+//		service.setCategory(new AppleCategory());
+//		service.getCategory().printCategory();
+		
+		// IoC Container
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		CategoryService service = (CategoryService) context.getBean("service");
 		service.getCategory().printCategory();
 	}
 }
